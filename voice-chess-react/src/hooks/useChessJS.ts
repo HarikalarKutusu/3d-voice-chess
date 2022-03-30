86a2efa037db226bff5c09a0e97207f82f67d92c
bozden <bulentozden2007@gmail.com>
2022-03-30 07:35:31 +0300

    Remove unused/leftover hook

--- voice-chess-react/src/hooks/useChessJS.ts
+++
@@ -1,7 +0,0 @@
-import { ChessInstance, Square } from "chess.js";
-import { useStore } from "./../stores/vcstore";
-
-export const useChessJSLegalMoves = (coord: Square) => {
-  const { chess } = useStore();
-  return chess?.moves({ square: coord });
-};
